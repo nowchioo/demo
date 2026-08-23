32b274e2af1fa9639164334244c2e020b9937e93
yl <719926241@qq.com>
2026-08-23 21:25:39 +0800

    开发完成

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: yl
+ * @Date: 2026-08-23 21:22:31
+ * @LastEditors: yl
+ * @LastEditTime: 2026-08-23 21:25:02
+ * @FilePath: \demo\src\App.vue
+ * @Description:
+-->
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 </script>
@@ -6,4 +14,7 @@ import { RouterView } from 'vue-router'
   <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped>
+.aaa{
+  color:#fff;
+}</style>
